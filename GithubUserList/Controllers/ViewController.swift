dd0107fd537d3844e764b930dbcb0e133deef919
John Walter Ramos <johnwalterramos17@gmail.com>
2020-12-03 18:08:45 +0800

    main #comment Initial commit structure

---
+++ GithubUserList/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubUserList
+//
+//  Created by John Walter Ramos on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
