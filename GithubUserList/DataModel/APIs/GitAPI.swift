d36c6b6ffaa87ccb10988a4231f51c763e7cbb09
John Walter Ramos <johnwalterramos17@gmail.com>
2020-12-03 19:05:53 +0800

    main #comment Added object class and list display controllers

---
+++ GithubUserList/DataModel/APIs/GitAPI.swift
@@ -0,0 +1,10 @@
+//
+//  GitAPI.swift
+//  GithubUserList
+//
+//  Created by John Walter Ramos on 12/3/20.
+//
+
+import Foundation
+
+let API_BASEURL = 'https://api.github.com/';
---
+++ GithubUserList/DataModel/Objects/GitUsers.swift
@@ -0,0 +1,30 @@
+//
+//  GitUsers.swift
+//  GithubUserList
+//
+//  Created by John Walter Ramos on 12/3/20.
+//
+
+import Foundation
+
+// Class Object for Github User Information
+class GitUsers: NSObject {
+    var txtLoginName: String!
+    var intUserId: Int!
+    var txtNodeId: String!
+    var txtAvatarURL: String!
+    var txtGravatarId: String!
+    var txtURL: String!
+    var txtHTMLUrl: String!
+    var txtFollowersURL: String!
+    var txtLFollowingURL: String!
+    var txtGistsURL: String!
+    var txtStarredURL: String!
+    var txtSubsURL: String!
+    var txtOrgURL: String!
+    var txtReposURL: String!
+    var txtEventsURL: String!
+    var txtReceivedEventsURL: String!
+    var txtUserType: String!
+    var txtSiteAdmin: Bool!
+}
